047425138a8a44e2194917476132f7bb66757100
noelpadam <noel.philip.aries@gmail.com>
2025-12-01 11:59:35 +0530

    add cron

--- api.ts
+++ api.ts
@@ -1,4 +1,26 @@
 import Elysia from "elysia";
+import { cron } from '@elysiajs/cron'
 
 export default () => new Elysia()
-  .get('/hello', () => ({ message: 'Hello from me..' }));
+  .get('/hello', () => ({ message: 'Hello from me..' }))
+  .use(
+		cron({
+			name: '2FHE',
+			pattern: '* * * /1 * *',
+			run() {
+				console.log('Heartbeat')
+			}
+		})
+	)
+	.get(
+		'/stop',
+		({
+			store: {
+				cron: { heartbeat }
+			}
+		}) => {
+			heartbeat.stop()
+
+			return 'Stop heartbeat'
+		}
+	);
